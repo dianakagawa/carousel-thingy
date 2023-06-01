4ded023801ec3c2b69d8738e7c859538df98a9a7
Diana Nakagawa <drcnakagawa@gmail.com>
2023-06-01 11:25:03 -0400

    Created Images and Colors components

---
+++ src/lib/components/Colors.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let color: string, text: string;
+</script>
+
+<div class="w-full h-full flex items-center" style="background-color: {color};">
+	<p class="text-white font-bold text-4xl px-8 bg-gray-950 bg-opacity-50">{text}</p>
+</div>
---
+++ src/lib/components/Images.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let url: string, description: string;
+</script>
+
+<div
+	style="background-image: url({url})"
+	class="w-full h-full bg-center bg-cover flex items-center"
+>
+	<p class="text-white font-bold text-4xl px-8 bg-gray-950 bg-opacity-50">
+		{description}
+	</p>
+</div>
